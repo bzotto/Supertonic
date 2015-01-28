ec8aa0bfb6b71aa7d47faa7dc57a060083d04136
bzotto <bzotto@gmail.com>
2015-01-27 17:36:40 -0800

    Initial Commit

---
+++ Supertonic/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Supertonic
+//
+//  Created by Ben Zotto on 1/27/15.
+//  Copyright (c) 2015 Ben Zotto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
